d0c1b61c12bbc0937db4b4a23dfb4069083fa573
timfargo <isalintimur981@gmail.com>
2022-05-14 09:45:03 +0300

    Initial commit

---
+++ src/sky/pro/Main.java
@@ -0,0 +1,53 @@
+package sky.pro;
+
+        public class Main {
+
+            public static void main(String[] args) {
+                // Задание 1
+//                int[] arr = new int[]{1, 2, 3};
+//                System.out.println("1,2,3");
+//
+//                float[] weight = new float[]{1.57f, 7.654f, 9.986f};
+//                System.out.println("1.57f,7.654f,9.986f");
+//
+//                int[] education = new int[11];
+//                education[0] = 333;
+//                int aprilEducation = education[0];
+//                System.out.println(aprilEducation);
+//
+//                // Задание 2
+//                int[] arr = new int[]{1, 2, 3};
+//                float[] weight = new float[]{1.57f, 7.654f, 9.986f};
+//                int[] eduction = new int[11];
+//               eduction[0] = 333;
+//               int aprilEduction = eduction[0];
+//                System.out.println("1,2,3");
+//                System.out.println("1.57f,7.654f,9.986");
+//                System.out.println(aprilEduction);
+//
+//                // Задание 3
+//                int[] arr = new int[]{3,2,1};
+//                float[] weight =new float[]{9.986f,7.654f,1.57f};
+//                int[] eduction = new int[11];
+//                eduction[0] =333;
+//                int aprilEduction = eduction[0];
+//               System.out.println("3,2,1");
+//                System.out.println("9.986f,7.654f,1.57f");
+//                       System.out.println(aprilEduction);
+
+                // Задание 4
+                int[] A = new int[1000];
+               for (int i = 2; i <=1000 ; i++) {
+                    System.out.println(i++);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
